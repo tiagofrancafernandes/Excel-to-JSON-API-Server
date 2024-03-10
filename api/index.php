7472158f3d2d146af665695b3dfc1ed8a7477479
Tiago França <tiago@tiagofranca.com>
2024-03-10 10:47:03 -0300

    change use class
    	modified:   api/index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Excel\Reader;
+
 require_once __DIR__ . '/../core/bootstrap.php';
 
 if (filter_var(request_any_get('tracy', false), FILTER_VALIDATE_BOOL)) {
     require_once __DIR__ . '/tracy.php';
 }
 
-App\Excel\Reader::response();
+Reader::response();
