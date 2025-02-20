924d0b927ffc2347b371af05a9aa9efa5a8fb59d
Tiago França <github@tiagofranca.com>
2025-02-20 21:31:42 +0000

    allow cache as public On branch task/allow-cache Changes to be committed: 	modified:   core/cache.php

--- core/cache.php
+++ core/cache.php
@@ -1,9 +1,17 @@
 <?php
 
-$threeHoursInSeconds = 3 * 60 * 60; // Calculate 3 hours in seconds
+$_publicCache = true;
 
-header("Cache-Control: public, max-age={$threeHoursInSeconds}"); // Public caching, max-age in seconds
-header("Expires: " . gmdate("D, d M Y H:i:s", time() + $threeHoursInSeconds) . " GMT"); // Expires timestamp in GMT
-header("Pragma: public"); // For compatibility with older browsers
-header("Last-Modified: " . gmdate("D, d M Y H:i:s", filemtime(__FILE__)) . " GMT"); // Last-modified timestamp (optional, good practice)
-// header("ETag: " . md5_file(__FILE__)); // ETag (optional, good practice)
+if ($_publicCache) {
+    header("Cache-Control: public"); // Indicates that the response can be cached
+}
+
+if (!$_publicCache) {
+    $threeHoursInSeconds = 3 * 60 * 60; // Calculate 3 hours in seconds
+
+    header("Cache-Control: public, max-age={$threeHoursInSeconds}"); // Public caching, max-age in seconds
+    header("Expires: " . gmdate("D, d M Y H:i:s", time() + $threeHoursInSeconds) . " GMT"); // Expires timestamp in GMT
+    header("Pragma: public"); // For compatibility with older browsers
+    // header("Last-Modified: " . gmdate("D, d M Y H:i:s", filemtime(__FILE__)) . " GMT"); // Last-modified timestamp (optional, good practice)
+    header("ETag: " . md5_file(__FILE__)); // ETag (optional, good practice)
+}
