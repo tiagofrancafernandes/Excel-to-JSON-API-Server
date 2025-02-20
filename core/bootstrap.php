a817597d2c985d9cbe76b3e10831695c09029d54
Tiago França <github@tiagofranca.com>
2025-02-20 21:21:21 +0000

    cache on response
    	modified:   core/bootstrap.php
    	new file:   core/cache.php

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,8 +4,15 @@ if (!defined('BASE_PATH')) {
     define('BASE_PATH', realpath(__DIR__ . '/..'));
 }
 
-require_once __DIR__ . '/cors.php';
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/../vendor/symfony/var-dumper/Resources/functions/dump.php';
+
+require_once __DIR__ . '/cors.php';
+
+$toCache = !filter_var(request_any_get('no-cache', false), FILTER_VALIDATE_BOOL, FILTER_NULL_ON_FAILURE);
+
+if ($toCache) {
+    require_once __DIR__ . '/cache.php';
+}
+
---
+++ core/cache.php
@@ -0,0 +1,9 @@
+<?php
+
+$threeHoursInSeconds = 3 * 60 * 60; // Calculate 3 hours in seconds
+
+header("Cache-Control: public, max-age={$threeHoursInSeconds}"); // Public caching, max-age in seconds
+header("Expires: " . gmdate("D, d M Y H:i:s", time() + $threeHoursInSeconds) . " GMT"); // Expires timestamp in GMT
+header("Pragma: public"); // For compatibility with older browsers
+header("Last-Modified: " . gmdate("D, d M Y H:i:s", filemtime(__FILE__)) . " GMT"); // Last-modified timestamp (optional, good practice)
+// header("ETag: " . md5_file(__FILE__)); // ETag (optional, good practice)
