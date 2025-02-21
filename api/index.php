4fefbab4e291ef71c8ae7416a19b8886038c4666
Tiago França <dev@tiagofranca.com>
2025-02-21 18:43:07 -0300

    ob_ fn set

--- api/index.php
+++ api/index.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+ob_start();
+
 use App\Excel\Reader;
 
 require_once __DIR__ . '/../core/bootstrap.php';
@@ -9,3 +12,4 @@ if (filter_var(request_any_get('tracy', false), FILTER_VALIDATE_BOOL)) {
 }
 
 Reader::response();
+ob_end_flush();
