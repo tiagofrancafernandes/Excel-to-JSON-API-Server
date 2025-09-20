424e8da4270f95ad7005ac8e237386c002ed1d3e
Tiago França <github@tiagofranca.com>
2025-09-20 19:06:02 -0300

    added ui/parser check

--- api/index.php
+++ api/index.php
@@ -7,6 +7,13 @@ use App\Excel\Reader;
 
 require_once __DIR__ . '/../core/bootstrap.php';
 
+if (filter_var(request_any_get('ui', false), FILTER_VALIDATE_BOOL) || filter_var(request_any_get('parser', false), FILTER_VALIDATE_BOOL)) {
+    $content = file_get_contents(__DIR__ . '/js-parser.html');
+
+    echo $content;
+    die;
+}
+
 if (filter_var(request_any_get('tracy', false), FILTER_VALIDATE_BOOL)) {
     require_once __DIR__ . '/tracy.php';
 }
